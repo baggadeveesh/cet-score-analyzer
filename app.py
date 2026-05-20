6f2737c906ac538c32e2dbc234602285ef5946a4
Deveesh Bagga <162809328+baggadeveesh@users.noreply.github.com>
2026-05-21 00:03:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, abort
 from bs4 import BeautifulSoup
 
 import os
